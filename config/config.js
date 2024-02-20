4bc788809c7ab04d5e6c869aee935f21345e20a0
Hyunkyung-Nam <gusrud13579@gmail.com>
2024-02-20 10:42:20 +0900

    config.js파일 수정

--- config/config.js
+++ config/config.js
@@ -17,10 +17,10 @@ module.exports = {
         dialect: 'mysql',
     },
     production: {
-        username: 'root',
-        password: null,
-        database: 'database_production',
-        host: '127.0.0.1',
+        username: process.env.PRODNAME,
+        password: process.env.PRODPW,
+        database: process.env.PRODDATABASE,
+        host: process.env.PRODHOST,
         dialect: 'mysql',
     },
 };
